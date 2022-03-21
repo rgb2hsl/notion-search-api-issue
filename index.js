15e8a3cd5febfdc0649795e5c26951903725a072
ru-web-designer <ru.web.designer@gmail.com>
2022-03-21 18:23:56 +0300

    initial

---
+++ index.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const notionToken = "secret_z1BsRMegzscnHqlPXcfC5RFs7OkcpEPhYPZJ52mHQZA";
+
+const options = {
+    method: 'post',
+    url: 'https://api.notion.com/v1/search',
+    headers: {
+        Accept: 'application/json',
+        'Notion-Version': '2022-02-22',
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${notionToken}`
+    },
+    data: {
+        query: "test",
+        page_size: 100
+    }
+};
+
+axios.request(options).then(function (response) {
+    console.log(response.data);
+}).catch(function (error) {
+    console.error(error);
+});
